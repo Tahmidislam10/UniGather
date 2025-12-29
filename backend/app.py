462878612559ff88cda9779a88567187fc27e1d1
Tahmid Islam <155205943+Tahmidislam10@users.noreply.github.com>
2025-12-29 21:40:24 +0000

    Update

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,44 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 from db import get_db
 from datetime import datetime
+from flask import jsonify
+from flask import send_from_directory
 
 app = Flask(__name__)
 db = get_db()
 events = db["events"]
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/events-page")
+def events_page():
+    return render_template("events.html")
+
+@app.route("/create")
+def create_page():
+    return render_template("create.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@app.get("/events")
+def get_all_events():
+    docs = list(events.find().sort("created_at", -1))
+    for d in docs:
+        d["_id"] = str(d["_id"])
+    return jsonify(docs), 200
+
+
 
 @app.post("/create/submit-event")
 def create_event():
@@ -24,5 +58,11 @@ def create_event():
 
     return f"Event created: {event_name}", 201
 
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+
+
