f8530587a08c77beacb7a7a60136623819ef7407
Steven <steven.chen@city.ac.uk>
2026-01-04 00:02:56 +0000

    Content Loaded Wait

--- static/analytics.js
+++ static/analytics.js
@@ -8,67 +8,73 @@ function renderCharts(labels, eventsData, attendeesData) {
         type: "bar",
         data: {
             labels: labels,
-            datasets: [{
-                label: "Events Hosted",
-                data: eventsData
-            }]
+            datasets: [
+                {
+                    label: "Events Hosted",
+                    data: eventsData,
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
 
     attendeesChart = new Chart(document.getElementById("attendeesChart"), {
         type: "bar",
         data: {
             labels: labels,
-            datasets: [{
-                label: "Attendees",
-                data: attendeesData
-            }]
+            datasets: [
+                {
+                    label: "Attendees",
+                    data: attendeesData,
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
+            responsive: true,
+        },
     });
 }
 
 function loadSummaryStats() {
     fetch("/api/analytics/summary")
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             document.getElementById("fillRate").innerText =
                 data.average_fill_rate + "%";
 
-            document.getElementById("bookingRatio").innerText =
-                `${data.booked} booked / ${data.waitlisted} waitlisted`;
+            document.getElementById(
+                "bookingRatio"
+            ).innerText = `${data.booked} booked / ${data.waitlisted} waitlisted`;
 
             document.getElementById("cancellations").innerText =
                 data.cancellations;
         });
 }
 
-
 function loadWeekly() {
     fetch("/api/analytics/weekly")
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             renderCharts(data.weeks, data.events, data.attendees);
         });
 }
 
 function loadDaily() {
     fetch("/api/analytics/daily")
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             renderCharts(data.days, data.events, data.attendees);
         });
 }
 
-document.getElementById("weeklyBtn").onclick = loadWeekly;
-document.getElementById("dailyBtn").onclick = loadDaily;
+// Waits for HTML document to finish loading
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("weeklyBtn").onclick = loadWeekly;
+    document.getElementById("dailyBtn").onclick = loadDaily;
 
-// Default view on page load
-loadSummaryStats();
-loadWeekly();
-
+    // Default view on page load
+    loadSummaryStats();
+    loadWeekly();
+});
